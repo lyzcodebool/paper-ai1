8fba0847042680b92fbe936e386b61871642a49e
刘彦志 <yanzhiliu@trip.com>
2024-03-15 12:13:25 +0800

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,36 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className}>
-    {}
+      {/* <Script>{`
+            if ('serviceWorker' in navigator) {
+              window.addEventListener('load', function() {
+                navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+                  // 注册成功
+                  console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                }, function(err) {
+                  // 注册失败 :(
+                  console.log('ServiceWorker registration failed: ', err);
+                });
+              });
+            }
+          `}</Script> */}
+      {/* msft clarify */}
+      <Script>
+        {`(function (c, l, a, r, i, t, y) {
+        c[a] =
+          c[a] ||
+          function () {
+            (c[a].q = c[a].q || []).push(arguments);
+          };
+        t = l.createElement(r);
+        t.async = 1;
+        t.src = "https://www.clarity.ms/tag/" + i;
+        y = l.getElementsByTagName(r)[0];
+        y.parentNode.insertBefore(t, y);
+      })(window, document, "clarity", "script", "l869naiex9");`}
+      </Script>
+      {/* google一键登录 */}
+
     </html>
   );
 }
