836aea13e319bc9d937cf2a6be4e6ea3e7a8e422
刘彦志 <yanzhiliu@trip.com>
2024-03-15 11:29:13 +0800

    1

--- components/SignUpUserSteps.tsx
+++ components/SignUpUserSteps.tsx
@@ -54,7 +54,7 @@ export default function SignUpUserSteps() {
   return (
     <ol className="flex flex-col gap-6">
       <Step title="Sign up your first user">
-        <p>
+{/*        <p>
           Head over to the{' '}
           <Link
             href="/login"
@@ -64,7 +64,7 @@ export default function SignUpUserSteps() {
           </Link>{' '}
           page and sign up your first user. It's okay if this is just you for
           now. Your awesome idea will have plenty of users later!
-        </p>
+        </p>*/}
       </Step>
 
       <Step title="Create some tables and insert some data">
