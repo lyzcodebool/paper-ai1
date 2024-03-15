726bb478f25ef543c0d40130bfc342cae57c4f0f
刘彦志 <yanzhiliu@trip.com>
2024-03-15 11:59:19 +0800

    1

--- app/store/slices/authSlice.ts
+++ app/store/slices/authSlice.ts
@@ -10,7 +10,7 @@ export interface APIState {
 }
 
 const initialState: APIState = {
-  apiKey: "sk-GHuPUV6ERD8wVmmr36FeB8D809D34d93Bb857c009f6aF9Fe", //sk-ffe19ebe9fa44d00884330ff1c18cf82
+  apiKey: "", //sk-ffe19ebe9fa44d00884330ff1c18cf82
   referencesRedux: [],
   editorContent: "",
   upsreamUrl: "https://api.ablai.top", //https://api.openai.com  https://one.caifree.com https://chatserver.3211000.xyz https://api.deepseek.com
